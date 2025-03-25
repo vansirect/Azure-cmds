86e34aeb31b2480475771f52a21f918bd77f143a
Tz <75564936+vansirect@users.noreply.github.com>
2025-03-25 10:25:16 +0300

    Create 11.3.js

---
+++ 11.3.js
@@ -0,0 +1,53 @@
+Create triggers and user-defined functions
+Pretriggers
+The following example shows how a pretrigger is used to validate the properties of an Azure Cosmos item that is being created. It adds a timestamp property to a newly added item if it doesn't contain one.
+function validateToDoItemTimestamp() {
+    var context = getContext();
+    var request = context.getRequest();
+
+    // item to be created in the current operation
+    var itemToCreate = request.getBody();
+
+    // validate properties
+    if (!("timestamp" in itemToCreate)) {
+        var ts = new Date();
+        itemToCreate["timestamp"] = ts.getTime();
+    }
+
+    // update the item that will be created
+    request.setBody(itemToCreate);
+}
+Post-triggers
+The following example shows a post-trigger. This trigger queries for the metadata item and updates it with details about the newly created item.
+
+function updateMetadata() {
+var context = getContext();
+var container = context.getCollection();
+var response = context.getResponse();
+
+// item that was created
+var createdItem = response.getBody();
+
+// query for metadata document
+var filterQuery = 'SELECT * FROM root r WHERE r.id = "_metadata"';
+var accept = container.queryDocuments(container.getSelfLink(), filterQuery,
+    updateMetadataCallback);
+if(!accept) throw "Unable to update metadata, abort";
+
+function updateMetadataCallback(err, items, responseOptions) {
+    if(err) throw new Error("Error" + err.message);
+        if(items.length != 1) throw 'Unable to find metadata document';
+
+        var metadataItem = items[0];
+
+        // update metadata
+        metadataItem.createdItems += 1;
+        metadataItem.createdNames += " " + createdItem.id;
+        var accept = container.replaceDocument(metadataItem._self,
+            metadataItem, function(err, itemReplaced) {
+                    if(err) throw "Unable to update metadata, abort";
+            });
+        if(!accept) throw "Unable to update metadata, abort";
+        return;
+    }
+}
