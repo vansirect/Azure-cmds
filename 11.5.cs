b2c52846d20de352f3935df973aa3ff02dfd5a74
Tz <75564936+vansirect@users.noreply.github.com>
2025-03-25 10:32:53 +0300

    Update 11.5.cs

--- 11.5.cs
+++ 11.5.cs
@@ -21,3 +21,31 @@ private static async Task<ChangeFeedProcessor> StartChangeFeedProcessorAsync(
     Console.WriteLine("Change Feed Processor started.");
     return changeFeedProcessor;
 }
+/// <summary>
+/// The delegate receives batches of changes as they are generated in the change feed and can process them.
+/// </summary>
+static async Task HandleChangesAsync(
+    ChangeFeedProcessorContext context,
+    IReadOnlyCollection<ToDoItem> changes,
+    CancellationToken cancellationToken)
+{
+    Console.WriteLine($"Started handling changes for lease {context.LeaseToken}...");
+    Console.WriteLine($"Change Feed request consumed {context.Headers.RequestCharge} RU.");
+    // SessionToken if needed to enforce Session consistency on another client instance
+    Console.WriteLine($"SessionToken ${context.Headers.Session}");
+
+    // We may want to track any operation's Diagnostics that took longer than some threshold
+    if (context.Diagnostics.GetClientElapsedTime() > TimeSpan.FromSeconds(1))
+    {
+        Console.WriteLine($"Change Feed request took longer than expected. Diagnostics:" + context.Diagnostics.ToString());
+    }
+
+    foreach (ToDoItem item in changes)
+    {
+        Console.WriteLine($"Detected operation for item with id {item.id}, created at {item.creationTime}.");
+        // Simulate some asynchronous operation
+        await Task.Delay(10);
+    }
+
+    Console.WriteLine("Finished handling changes.");
+}
